0330cd0816e5d08f068a61bb1bb7269655b19934
Jessa Marie Barre <jmlbarre@gmail.com>
2017-09-08 11:49:14 -0400

    created filter field

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import FilterField from '../containers/FilterField'
+
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
+    <FilterField />
     <Footer />
   </div>
 )
