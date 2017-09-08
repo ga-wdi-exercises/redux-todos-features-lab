7d19542b5854f0655125c82d3a14b32a1d755d1f
MATT FILBERT <mcfilbert@email.wm.edu>
2017-09-08 11:46:26 -0400

    yup!

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,7 +1,10 @@
+// const initialState = { filter: 'SHOW_ALL' }
+// const visibilityFilter = (state = initialState, action) => {
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter
+      // query: action.query
     default:
       return state
   }
